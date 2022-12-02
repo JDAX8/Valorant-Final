4ee35de0afff41e4ee393e3b54b34f3ec214c326
JDAX8 <juanampudia888@gmail.com>
2022-12-01 19:59:21 -0500

    final

---
+++ JS/map.js
@@ -0,0 +1,36 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const valid = urlParams.get('uuid')
+const map = document.getElementById(`mapa`)
+const urlbase = 'https://valorant-api.com/v1/maps/'
+
+async function getmapInfo(url){
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data)
+
+
+map.innerHTML += `
+<div>
+        <h1 class="M">${data.data.displayName}</h1>
+    </div>
+    <div class="parrafo ">
+            <img class="Ra" src="${data.data.splash}" alt="">
+            <p class="JR">${data.data.displayName} es un mapa que está en las coordenadas ${data.data.coordinates} en donde dos equipos luchan a muerte por defender o atacar los sites A (en las cordenadas X ${data.data.callouts[9].location.x} Y ${data.data.callouts[9].location.y}) y B (en las cordenadas X ${data.data.callouts[19].location.x} Y ${data.data.callouts[19].location.y}) </p>
+
+            <img class="Ram" src="${data.data.displayIcon}">
+
+       <a href="#boton"><button class="boton">
+            <img src="../IMG/Save.png" height="50px" width="50px" alt="">
+        </button></a>
+
+    </div>
+`
+}
+async function getallmapInfo(){
+    let url = urlbase + valid;
+    await getmapInfo(url)
+}
+
+getallmapInfo()
---
+++ JS/script.js
@@ -0,0 +1,23 @@
+const productsContainerDiv = document.getElementById("pcontainer")
+var productListHtml = ""
+
+function fetchAsync() {
+    const url = 'https://valorant-api.com/v1/maps'
+    console.log(url)
+    fetch(url)
+        .then(response => {
+            response.json().then(data => {
+                data.data.forEach(information => {
+                    document.getElementById("pcontainer").innerHTML += `
+                    <a href="map.html?uuid=${information.uuid}"><img class="img" src="${information.splash}"" ></a>
+                    `
+                  })
+                console.log(data)
+                
+            })
+        })
+        
+}
+fetchAsync()
+
+
